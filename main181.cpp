ba81c2a0aae2fff7a6e53eaf8ea777d2a6652c80
BR4ECJlAB <xxxfat32xxx@gmail.com>
2021-11-01 18:51:20 +0300

    01.11.2021
    01.11.2021

--- main181.cpp
+++ main181.cpp
@@ -14,12 +14,13 @@ void on_matrix_size(int number)
 	std::cout << "On matrix_size: " << number << '\n';
 }
 
-void main(int argc, const char* argv[]) {
+void main(int argc, const char* argv[]){
+	setlocale(LC_ALL, "rus");
 	srand(time(NULL));
-	int default_size = 5;//стандартные значени€ размеров 
+	int default_size = 17;//стандартные значени€ размеров 
 	int array_size = default_size;
-	int matrix_size = default_size;
-
+	int matrix_size = 7;
+	
 	try//работа через командную строку
 	{
 		options_description desc{ "Options" };
@@ -44,7 +45,7 @@ void main(int argc, const char* argv[]) {
 			std::cout << "Matrix_size: " << vm["matrix_size"].as<int>() << '\n';
 			matrix_size = vm["matrix_size"].as<int>();//записываем в наш размер значение из командной строки
 		}
-
+		
 	}
 	catch (const error& ex)
 	{
@@ -52,21 +53,38 @@ void main(int argc, const char* argv[]) {
 	}
 
 	//сама работа с векторами и матрицами
-	double* array_int_time = new double[array_size];
-	double* array_int_data = new double[array_size];
-	double** matrix_int_time = new double*[matrix_size];
-	double** matrix_int_data = new double*[matrix_size];
-	for (int i = 0; i < matrix_size; i++) {
-		matrix_int_time[i] = new double[matrix_size];
-		matrix_int_data[i] = new double[matrix_size];
+	double *array_int_time = new double[array_size];
+	double *array_int_data = new double[array_size];
+	double **matrix_int_time = new double*[matrix_size];
+	double **matrix_int_data = new double*[matrix_size];
+	for(int i = 0; i < matrix_size; i++) {
+		matrix_int_time[i]=new double[matrix_size]; 
+		matrix_int_data[i]=new double[matrix_size]; 
 	}
 
-	initialization(array_int_time, array_int_data, array_size, matrix_int_time, matrix_int_data, matrix_size);
-	sum_array(array_int_time, array_int_data, array_size);
+	initialization(array_int_time, array_int_data, array_size,matrix_int_time, matrix_int_data, matrix_size);
+	printf("¬ектор data: \n");
 	output_array(array_int_data, array_size);
-	sum_matrix(matrix_int_time, matrix_int_data, matrix_size);
-	output_matrix(matrix_int_data, matrix_size);
+	printf("¬ектор time: \n");
+	output_array(array_int_time, array_size);
+	printf("ѕрибавление к элементам первого вектора, элементов второго вектора: \n");
+	sum_array(array_int_data, array_int_time, array_size, array_size);
+	output_array(array_int_data, array_size);
+	printf("¬ычитание веторов: \n");
+	difference_array(array_int_data, array_int_time, array_size, array_size);
+	output_array(array_int_data, array_size);
+	printf("ѕрибавление к элементам первого вектора, элементов второго вектора начина€ с элемента 5: \n");
+	sum_array(array_int_data, array_int_time, array_size, array_size,5);
+	output_array(array_int_data, array_size);
 
+	printf("матрица data: \n");
+	output_matrix(matrix_int_data, matrix_size);
+	printf("матрица time: \n");
+	output_matrix(matrix_int_time, matrix_size);
+	printf("ѕрибавление к элементам первой матрицы, элементов второй матрицы: \n");
+	sum_matrix(matrix_int_data, matrix_int_time, matrix_size);
+	output_matrix(matrix_int_data, matrix_size);
+	
 	system("pause");
 }
 
